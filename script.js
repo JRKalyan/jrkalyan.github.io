f94e4e06dbf2061726b8a75d18b9e306290bcf0d
John <john.r.kalyan@gmail.com>
2017-03-20 00:14:26 -0400

    adding jquery/js files

---
+++ script.js
@@ -0,0 +1,9 @@
+function prepEventHandlers() {
+    // initialize all your event handlers within here
+}
+
+window.onload = function() {
+    // loads all event handlers
+    prepEventHandlers();
+};
+
