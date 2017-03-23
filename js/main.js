12239d8f7c540f5ecb898ff240f50aaa68a65642
John <john.r.kalyan@gmail.com>
2017-03-23 01:25:43 -0400

    finished jquery functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-var articleIndex = 0;
-var articles = undefined;
+var currentArt = 0;
+
+function changeArt(index) {
+    if (index != currentArt) {
+        $(".block:eq(" + index + ")").animate({top: '10%'}, 500);
+        $(".block:eq(" + currentArt + ")").animate({left: '100%'}, 400, function() {
+            $(this).css({
+                "top": "100%",
+                "left": "0%"
+            })
+        });
+        currentArt = index;
+    }
+}
 
 $("document").ready(function() {
 
@@ -9,27 +21,23 @@ $("document").ready(function() {
 
     $(".block").css({
         "position": "absolute",
-        "top": "150%",
+        "top": "100%",
         "left": "0px",
-        "margin-left": "20%",
+        "margin-left": "15%",
     });
+
+    $(".about").css({
+        "top": "10%"
+    })
 
     $("#pickSection").css("visibility", "visible");
 
     // --------------------------------------------------------------
 
-    articles = document.getElementsByClassName("block");
-
-    $(".navButtons").click(function(){
-        $(".about").animate({
-            top: '0%',
-
-    });
-        console.log("hello");
-    })
-
-
-
+    document.getElementById("about").onclick = function() {changeArt(0);};
+    document.getElementById("projects").onclick = function() {changeArt(2);};
+    document.getElementById("skills").onclick = function() {changeArt(1);};
+    document.getElementById("contact").onclick = function() {changeArt(3);};
 
 
 });
